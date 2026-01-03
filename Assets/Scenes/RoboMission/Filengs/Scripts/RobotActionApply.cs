705f25f880fe5163609608a92ca1b9f494be6ea3
ColdFox_ <brattsev11@gmail.com>
2026-01-03 22:39:55 +0200

    ChangedEnum+added error & breaks

--- Assets/Scenes/RoboMission/Filengs/Scripts/RobotActionApply.cs
+++ Assets/Scenes/RoboMission/Filengs/Scripts/RobotActionApply.cs
@@ -1,5 +1,11 @@
 using System.Collections; using System.Collections.Generic; using UnityEngine;
 
 public class RobotActionApply : MonoBehaviour {
-    
+    public void ApplyAction(string Action) {
+        switch (Action) {
+            case "Move" : break;
+            case "Charge" : break;
+            default: Debug.Log("FalseCall to the action script."); break;
+        }
+    }
 }
--- Assets/Scenes/RoboMission/Filengs/Scripts/RobotActions.cs
+++ Assets/Scenes/RoboMission/Filengs/Scripts/RobotActions.cs
@@ -1,6 +1,4 @@
 public enum RobotActions {
-    MoveRight,
-    MoveUp,
-    MoveDown,
-    MoveLeft
+    Move,
+    Charge
 }
