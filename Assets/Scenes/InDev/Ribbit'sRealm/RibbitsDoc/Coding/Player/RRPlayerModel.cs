0a93d4a42a504b55ffede7b0c610d2c4568becb8
ColdFox_ <brattsev11@gmail.com>
2026-02-14 13:32:43 +0200

    day3{1}

--- Assets/Scenes/InDev/Ribbit'sRealm/RibbitsDoc/Coding/Player/RRPlayerModel.cs
+++ Assets/Scenes/InDev/Ribbit'sRealm/RibbitsDoc/Coding/Player/RRPlayerModel.cs
@@ -2,13 +2,20 @@ using System.Collections; using System.Collections.Generic; using UnityEngine;
 
 public class RRPlayerModel : MonoBehaviour {
     public GameObject[] BodyParts;
+    public float LookBoarder = 30;
     RRPlayer Player;
 
     void Start() {
         Player = transform.parent.GetComponent<RRPlayer>();
     }
     void Update() {
-        Quaternion camRot = Player.Cam.transform.rotation;
-        //BodyParts[0].transform.rotation = new Quaternion(-camRot.x,-camRot.y,-camRot.z,-camRot.w);
+        float xRotation = Player.Cam.transform.localEulerAngles.x;
+
+            if (xRotation > 180f) {
+                xRotation -= 360f;
+            }
+    
+        float clampedX = Mathf.Clamp(xRotation, -LookBoarder, LookBoarder);
+        BodyParts[0].transform.localEulerAngles = new Vector3(-clampedX, 0, 0);
     }
 }
